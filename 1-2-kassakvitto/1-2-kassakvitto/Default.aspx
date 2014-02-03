872a3325c321c11c173f6bb5b9e51adbbc30bb2a
Juhani <ja222qm@lnu.student.se>
2014-02-03 13:13:52 +0200

    1-2 kassakvitto påbörjad

---
+++ 1-2-kassakvitto/1-2-kassakvitto/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_1_2_kassakvitto.Default" ViewStateMode="Disabled" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Kassakvitto</title>
+</head>
+<body>
+    <form id="form1" runat="server" defaultbutton="Getdiscount" defaultfocus="Amount">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Kassakvitto"></asp:Label>
+        <asp:Label ID="Label2" runat="server" Text="Total köpesumma"></asp:Label>
+        <asp:TextBox ID="Amount" runat="server" TextMode="SingleLine"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Total köpesumma får inte vara tom!" ControlToValidate="Amount"></asp:RequiredFieldValidator>
+        <asp:CompareValidator ID="CompareValidator1" runat="server" ErrorMessage="Total köpesumma måste innehålla ett flyttal som är större än noll" ControlToValidate="Amount" Operator="GreaterThan" Type="Double" ValueToCompare="0"></asp:CompareValidator>
+        <asp:Button ID="Getdiscount" runat="server" Text="Beräkna rabatt" OnClick="Getdiscount_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ 1-2-kassakvitto/1-2-kassakvitto/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("_1_2_kassakvitto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("_1_2_kassakvitto")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d80b3af5-f503-4519-9849-e65bbb753beb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
